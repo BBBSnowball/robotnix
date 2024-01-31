c5cd53ea197898777db5fd43bbb36e850690b218
Benjamin Koch <bbbsnowball@gmail.com>
2024-01-31 03:43:10 +0100

    fixup: don't touch faillog because that could trigger a bug in Docker/Go

--- bla.sh
+++ bla.sh
@@ -6,5 +6,5 @@ docker build --file Dockerfile1 --tag x1 .
 docker run -it --mount type=bind,source=/nix,target=/nix x1
 
 #docker build --file Dockerfile2 --tag x2 .
-docker build --file Dockerfile3 --tag x3 .
+docker build --file Dockerfile-initial-clone --tag x5 .
 
