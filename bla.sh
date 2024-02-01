6dcc21dec2ad186673899687b778bf3377a26e95
Benjamin Koch <bbbsnowball@gmail.com>
2024-02-01 03:20:18 +0100

    continue

--- bla.sh
+++ bla.sh
@@ -7,4 +7,6 @@ docker run -it --mount type=bind,source=/nix,target=/nix x1
 
 #docker build --file Dockerfile2 --tag x2 .
 docker build --file Dockerfile-initial-clone --tag x5 .
+docker build --file Dockerfile-clone-tag --tag x6 .
+docker build --file Dockerfile-shell-start2 --tag x7 .
 
