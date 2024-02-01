550b3ad6a41e723cc319549d90241ef5c0988e1f
Benjamin Koch <bbbsnowball@gmail.com>
2024-02-01 23:45:53 +0100

    add dockerfile for testing log limits

--- bla.sh
+++ bla.sh
@@ -5,12 +5,16 @@ docker build --file Dockerfile-build-tools --tag x1 .
 
 docker run -it --mount type=bind,source=/nix,target=/nix x1
 
+# How to debug a failed build?
+# -> https://github.com/docker/buildx/blob/v0.11.2/docs/guides/debugging.md
+export BUILDX_EXPERIMENTAL=1
+
 #docker build --file Dockerfile2 --tag x2 .
 docker build --file Dockerfile-initial-clone --tag x5 .
 docker build --file Dockerfile-clone-tag --tag x6 .
 # rename x6b to x6: docker image tag x6b x6
 docker build --file Dockerfile-shell-start2 --tag x7 .
-docker build --file Dockerfile-build --tag x8 . --progress=plain
+docker build --file Dockerfile-build --tag x8 . --progress=plain --invoke on-error
 docker build --file Dockerfile-save-caches --tag x9 . --build-arg cache_buster=4 && docker run -it x9 find /cache -maxdepth 2
 
 #FIXME combine many of these files and use "--target" to stop early for development
