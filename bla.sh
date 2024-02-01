2f8770a14d666d938f6737fc894e9848a6857ab7
Benjamin Koch <bbbsnowball@gmail.com>
2024-02-01 23:52:33 +0100

    improve debuggability of failed builds (untested)

--- bla.sh
+++ bla.sh
@@ -5,6 +5,8 @@ docker build --file Dockerfile-build-tools --tag x1 .
 
 docker run -it --mount type=bind,source=/nix,target=/nix x1
 
+docker build --file Dockerfile-test-log-limit . --progress=plain
+
 # How to debug a failed build?
 # -> https://github.com/docker/buildx/blob/v0.11.2/docs/guides/debugging.md
 export BUILDX_EXPERIMENTAL=1
@@ -20,4 +22,6 @@ docker build --file Dockerfile-save-caches --tag x9 . --build-arg cache_buster=4
 #FIXME combine many of these files and use "--target" to stop early for development
 #  https://docs.docker.com/build/building/multi-stage/#stop-at-a-specific-build-stage
 #  -> Vermutlich eins mit dem "will ich immmer neu machen" und x5 und x6 separat.
+
+#FIXME We want to mount /nix into the container for later build steps. Can we do that or only when running a container..?
 
