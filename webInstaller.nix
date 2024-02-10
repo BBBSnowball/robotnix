9218406353aa5791895f4b6876820a2e8de66800
Benjamin Koch <bbbsnowball@gmail.com>
2024-02-10 03:14:04 +0100

    better way of combining web installer with factory images

--- webInstaller.nix
+++ webInstaller.nix
@@ -1,12 +1,15 @@
 # Usage:
-#   nix-build webInstaller.nix
-#   mkdir /tmp/groot-releases && echo abc >/tmp/groot-releases/bluejay-stable
-#   cp /path/to/release.zip /tmp/groot-releases/bluejay-factory-abc.zip
-#   nix run nixpkgs#python3 -- -m http.server -d result
-#   open http://localhost:8000/install/web.html
+#   mkdir -p x/releases
+#   ln -s result/index.html x/
+#   ln -s result/static x/
+#   echo abc >x/releases/bluejay-stable
+#   cp /path/to/release.zip x/releases/bluejay-factory-abc.zip
+#   nix-build webInstaller.nix -o x/result
+#   nix run nixpkgs#python3 -- -m http.server -d x
+#   open http://localhost:8000/
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "groot-web-install";
   version = "0.1~git-8f4a95";
 
@@ -19,16 +22,36 @@ stdenv.mkDerivation {
 
   #RELEASES_URL = "https://groot.example.com";
   RELEASES_URL = "/releases";
-  SYMLINK_RELEASES = "/tmp/groot-releases";
 
   nativeBuildInputs = [
     openssl
     (python3.withPackages (p: with p; [ gixy lxml cssselect jinja2 ]))
   ];
 
+  header = ''
+    <header>
+      <h1>
+        Fork of the web installer.
+        Most links won't work or point you to upstream info, which probably
+        doesn't apply - especially any contact info or where to report bugs!
+      </h1>
+      Release images will be downloaded from: <pre>${RELEASES_URL}</pre>
+    </header>
+  '';
+  passAsFile = [ "header" ];
+
   patchPhase = ''
     substituteInPlace static/js/web-install.js \
-      --replace https://releases.grapheneos.org "$RELEASES_URL"
+      --replace https://releases.grapheneos.org "$RELEASES_URL" \
+      --replace '"/js/' '"/static/js/'
+
+    substituteInPlace static/main.css \
+      --replace '"/fonts/' '"/static/fonts/'
+
+    # replace header and footer because the links won't work
+    # and the contact info doesn't apply for our fork
+    cp $headerPath templates/header.html
+    echo "" >templates/footer.html
   '';
 
   buildPhase = ''
@@ -41,7 +64,7 @@ stdenv.mkDerivation {
         hash=$(sha256sum "$file" | head -c 8)
         sri_hash=sha256-$(openssl dgst -sha256 -binary "$file" | openssl base64 -A)
         dest="$(dirname $file)/$hash.$(basename $file)"
-    
+
         if [[ $file == *.css ]]; then
             replace+=";s@\[\[css|/''${file#*/}\]\]@<link rel=\"stylesheet\" href=\"/''${dest#*/}\" integrity=\"$sri_hash\"/>@g"
         elif [[ $file == *.js ]]; then
@@ -52,14 +75,16 @@ stdenv.mkDerivation {
         replace+=";s@\[\[integrity|/''${file#*/}\]\]@''${sri_hash}@g"
         replace+=";s@\[\[path|/''${file#*/}\]\]@/''${dest#*/}@g"
     done
-    ( set -x; sed -i "$replace" static/**/*.html )
+    sed -i "$replace" static/**/*.html
+
+    substituteInPlace static/install/web.html \
+      --replace 'href="/' 'href="static/' \
+      --replace 'src="/' 'src="static/'
   '';
 
   installPhase = ''
-    cp -r static $out
-
-    if [ -n "$SYMLINK_RELEASES" ] ; then
-      ln -s "$SYMLINK_RELEASES" $out/releases
-    fi
+    mkdir $out $out/static
+    cp -r static/install/web.html $out/index.html
+    cp -r static/{js,fonts,apple-touch-icon.png,favicon.svg,favicon.ico,*.css} $out/static/
   '';
 }
