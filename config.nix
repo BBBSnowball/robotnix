e4e1f8871ffb704f18e199ce161c119402be57a2
Benjamin Koch <bbbsnowball@gmail.com>
2024-02-11 17:02:59 +0100

    clean up

--- config.nix
+++ config.nix
@@ -71,15 +71,9 @@ with lib;
   };
 
   # patch Updater URL
-  #FIXME robotnix does it with resources."packages/apps/Updater".url=url - can we do the same?
-  #    -> see https://source.android.com/docs/setup/create/new-device#use-resource-overlays
+  #FIXME Make this a proper mkIf for variant grapheneos-docker.
   apps.updater.url = "http://192.168.89.140:8000/groot-releases";
-  source.dirs."packages/apps/Updater" = mkIf false {
-    onlyPatches = true;
-    postPatch = ''
-      sed -i '/name="url"/ s@>.*<@>${config.apps.updater.url}<@' res/values/config.xml
-    '';
-  };
+  # see https://source.android.com/docs/setup/create/new-device#use-resource-overlays
   resources."packages/apps/Updater" = {
     url = config.apps.updater.url;
     channel_default = config.channel;
